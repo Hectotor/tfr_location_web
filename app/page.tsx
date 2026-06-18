34bd7d802fc6910230e33af173dbb3045b0b14bb
Hector Chablis <hector.chablis@gmail.com>
2026-06-18 23:18:00 +0200

    OK

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Avantages from './components/Avantages'
-import Flotte from './components/Flotte'
-import PourquoiNous from './components/PourquoiNous'
-import Temoignages from './components/Temoignages'
-import Localisation from './components/Localisation'
-import CTA from './components/CTA'
-import Footer from './components/Footer'
-import ScrollBar from './components/ScrollBar'
-import WhatsAppButton from './components/WhatsAppButton'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/maintenance')
+  }, [router])
+
   return (
-    <div className="min-h-screen bg-black text-white">
-      <ScrollBar />
-      <Header />
-      <Hero />
-      <Flotte />
-      <PourquoiNous />
-      <Temoignages />
-      <Localisation />
-      <CTA />
-      <Footer />
-      <WhatsAppButton />
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <p>Redirection vers la page de maintenance...</p>
     </div>
   )
 }
